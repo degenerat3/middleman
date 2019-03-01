ae06d77a6d854098d75407101119c17cae179f42
Jim Maskelony <james.maskelony@gmail.com>
2019-02-28 23:37:11 -0500

    Revert "Micah dev"

--- app.py
+++
@@ -1,9 +0,0 @@
-"""
-Process POST requests from clients, log data to files so it can be sent with filebeat
-@author: degenerat3
-"""
-
-from app import app
-
-if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
